f215c96435249860d9e20d667a7acd7aaf40c296
Zacard274 <176560418@qq.com>
2017-05-25 10:43:57 +0800

    study chan and goroutime

--- chan_gorou.go
+++ chan_gorou.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	var i = 3
+	ch := make(chan int)
 	go func(a int) {
 		fmt.Println(a)
+		v := <-ch
+		fmt.Println(v)
 	}(i)
+	ch <- 4
 	fmt.Println("1")
-	time.Sleep(1 * time.Second)
+
 }
